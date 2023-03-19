b8e22e7dbdf73298c63e7b852052e6881b2217a6
unknown <mohamedkhlifadd@gmail.com>
2023-03-20 00:08:57 +0200

    mohamed ibrahem

--- Dtos/Class1.cs
+++ Dtos/Class1.cs
@@ -5,6 +5,8 @@
         //hrlloooooooooooooo ya boyyyyyyyyyyyyyyyyyyyyye
 
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string test { get; set; }
+        public int age { get; set; }
+
     }
 }
